dd6edc86601f18a16ddb056224744df1b91256de
xcube <xcube16@gmail.com>
2019-11-02 22:41:01 -0500

    Ice and Fire dragons now wake up when hit

--- src/main/java/io/github/xcube16/iseedragons/ISeeDragons.java
+++ src/main/java/io/github/xcube16/iseedragons/ISeeDragons.java
@@ -4,6 +4,7 @@ import com.google.common.collect.BiMap;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -24,18 +25,19 @@ public class ISeeDragons {
     public static final String VERSION = "0.3";
     public static final Logger logger = LogManager.getLogger(NAME);
 
-
     @Nullable // lazy init
     private Method dragonSetSleeping;
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-        System.out.println(MODID + " dummy mod loaded");
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
+        logger.info("Scanning for Ice and Fire dragons...");
+        boolean foundOne = false;
         try {
             Field regField = EntityRegistry.instance().getClass().getDeclaredField("entityClassRegistrations");
             regField.setAccessible(true);
@@ -44,6 +46,7 @@ public class ISeeDragons {
             for (EntityRegistry.EntityRegistration entity : reg.values()) {
                 //logger.info(entity.getRegistryName().toString());
                 if (this.isDragon(entity.getRegistryName())) {
+                    foundOne = true;
                     logger.info("Fixed " + entity.getRegistryName() + " tracking distance");
                     Field rangeField = entity.getClass().getDeclaredField("trackingRange");
                     rangeField.setAccessible(true);
@@ -53,15 +56,20 @@ public class ISeeDragons {
         } catch (Exception e) {
             logger.error("Failed to fix Ice and Fire entity tracking distance", e);
         }
+        if (!foundOne) {
+            logger.warn("No Ice and Fire dragons found! Is it installed?");
+        }
     }
 
     @SubscribeEvent
     public void livingEntityAttacked(LivingAttackEvent e) {
-        //String id = EntityRegistry.getEntry(e.getEntity().getClass()).getRegistryName();
+        //EntityRegistry.getEntry(e.getEntity().getClass()).getRegistryName();
+
+        //note: EntityList.getKey(e.getEntity()) is null when a/the player is hit
         if (this.isDragon(EntityList.getKey(e.getEntity())) && e.getAmount() > 0.0F) {
             try {
                 if (this.dragonSetSleeping == null) {
-                    this.dragonSetSleeping = e.getEntity().getClass().getDeclaredMethod("setSleeping");
+                    this.dragonSetSleeping = this.getMethodInHierarchy(e.getEntity().getClass(), "setSleeping", boolean.class);
                     this.dragonSetSleeping.setAccessible(true); // it was public last I checked, but lets just do this anyway
                 }
                 this.dragonSetSleeping.invoke(e.getEntity(), false);
@@ -75,9 +83,20 @@ public class ISeeDragons {
         if (id == null) {
             return false;
         }
-        return id.getResourceDomain().equals("icdandfire") && (
+
+        return id.getResourceDomain().equals("iceandfire") && (
                 id.getResourcePath().equals("icedragon") ||
                 id.getResourcePath().equals("firedragon"));
     }
 
+    private Method getMethodInHierarchy(Class<?> clazz, String name, Class<?>... parameterTypes) throws NoSuchMethodException {
+        try {
+            return clazz.getDeclaredMethod(name, parameterTypes);
+        } catch (NoSuchMethodException e) {
+            if (clazz.getSuperclass() != null) {
+                return getMethodInHierarchy(clazz.getSuperclass(), name, parameterTypes);
+            }
+            throw e;
+        }
+    }
 }
