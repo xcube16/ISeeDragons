9199449769076fa0ed9d10358de0386bfe6b7ecf
xcube <xcube16@gmail.com>
2019-11-02 19:14:37 -0500

    OMG! Shivaxi noticed me! ... TODO: Fix dragonbone axe, fix sea serpent armor repair, fix dragons getting stuck in water. Some tiny clean ups Trying a fix for dragons not waking up when being attacked

--- src/main/java/io/github/xcube16/iseedragons/ISeeDragons.java
+++ src/main/java/io/github/xcube16/iseedragons/ISeeDragons.java
@@ -2,21 +2,31 @@ package io.github.xcube16.iseedragons;
 
 import com.google.common.collect.BiMap;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.annotation.Nullable;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 @Mod(modid= ISeeDragons.MODID, version = ISeeDragons.VERSION, acceptableRemoteVersions = "*", name = ISeeDragons.NAME)
 public class ISeeDragons {
     public static final String MODID = "iseedragons";
     public static final String NAME = "ISeeDragons";
-    public static final String VERSION = "0.2";
+    public static final String VERSION = "0.3";
     public static final Logger logger = LogManager.getLogger(NAME);
+
+
+    @Nullable // lazy init
+    private Method dragonSetSleeping;
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
@@ -32,8 +42,8 @@ public class ISeeDragons {
             BiMap<Class<? extends Entity>, EntityRegistry.EntityRegistration> reg =
                     (BiMap<Class<? extends Entity>, EntityRegistry.EntityRegistration>)regField.get(EntityRegistry.instance());
             for (EntityRegistry.EntityRegistration entity : reg.values()) {
-                logger.info(entity.getRegistryName().toString());
-                if (entity.getRegistryName().toString().equals("iceandfire:icedragon") || entity.getRegistryName().toString().equals("iceandfire:firedragon")) {
+                //logger.info(entity.getRegistryName().toString());
+                if (this.isDragon(entity.getRegistryName())) {
                     logger.info("Fixed " + entity.getRegistryName() + " tracking distance");
                     Field rangeField = entity.getClass().getDeclaredField("trackingRange");
                     rangeField.setAccessible(true);
@@ -41,8 +51,33 @@ public class ISeeDragons {
                 }
             }
         } catch (Exception e) {
-            logger.error("Failed to fix Ice and Fire entity tracking distance");
-            e.printStackTrace();
+            logger.error("Failed to fix Ice and Fire entity tracking distance", e);
         }
     }
+
+    @SubscribeEvent
+    public void livingEntityAttacked(LivingAttackEvent e) {
+        //String id = EntityRegistry.getEntry(e.getEntity().getClass()).getRegistryName();
+        if (this.isDragon(EntityList.getKey(e.getEntity())) && e.getAmount() > 0.0F) {
+            try {
+                if (this.dragonSetSleeping == null) {
+                    this.dragonSetSleeping = e.getEntity().getClass().getDeclaredMethod("setSleeping");
+                    this.dragonSetSleeping.setAccessible(true); // it was public last I checked, but lets just do this anyway
+                }
+                this.dragonSetSleeping.invoke(e.getEntity(), false);
+            } catch (Exception ex) {
+                logger.error("Failed to wake up Ice and Fire dragon", ex);
+            }
+        }
+    }
+
+    private boolean isDragon(@Nullable ResourceLocation id) {
+        if (id == null) {
+            return false;
+        }
+        return id.getResourceDomain().equals("icdandfire") && (
+                id.getResourcePath().equals("icedragon") ||
+                id.getResourcePath().equals("firedragon"));
+    }
+
 }
