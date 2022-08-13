289111a919e1ea9893501bf336dc5094719a34e3
xcube <xcube16@gmail.com>
2022-08-12 21:17:44 -0500

    Increment version number

--- src/main/java/io/github/xcube16/iseedragons/ISD.java
+++ src/main/java/io/github/xcube16/iseedragons/ISD.java
@@ -9,6 +9,6 @@ import org.apache.logging.log4j.Logger;
 public final class ISD {
     public static final String MODID = "iseedragons";
     public static final String NAME = "ISeeDragons";
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.2.1";
     public static final Logger logger = LogManager.getLogger(NAME);
 }
